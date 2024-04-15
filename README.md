6fd575489bd417b19b48f06ee5082f6fbba82695
CTix <164232392+cmtix@users.noreply.github.com>
2024-04-15 10:56:53 -0800

    Need spellcheck

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Hello-World 
 # This is a new comment. 
-#This is a new new comment. 
+# This is a new new comment. 
 # Third time comment's the charm? 
+# My spelling is terrible...
--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
 # testing out python 
 print("Hello World!") 
 # This is a new comment as well. 
-print("I made it to Monday and all I got was this repositiory.") 
+print("I made it to Monday and all I got was this repository.") 
